b0b45e68ad36d4ab3559ec40a6c170f770e3e0fc
Neerajais <kamal84041@gmail.com>
2025-08-31 12:32:52 +0530

    Deployement commit 1.2

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,8 @@ import joblib
 from sklearn.ensemble import RandomForestClassifier
 class TestModelTraining(unittest.TestCase):
    def test_model_training(self):
-      model = joblib.load('model/iris_model.pkl')self.assertIsInstance(model, RandomForestClassifier)
+      model = joblib.load('model/iris_model.pkl')
+      self.assertIsInstance(model, RandomForestClassifier)
       self.assertGreaterEqual(len(model.feature_importances_), 4)
 
 if __name__ == '__main__':
